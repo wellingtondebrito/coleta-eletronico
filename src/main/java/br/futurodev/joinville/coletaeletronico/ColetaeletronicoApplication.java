5ef6c99cb8371677fcf31d02a87c530f001b1f04
wellingtondebrito <wellingtondebritosouza@gmail.com>
2025-04-24 11:47:34 -0300

    feat: inicia projeto de coleta de lixo eletronico

---
+++ src/main/java/br/futurodev/joinville/coletaeletronico/ColetaeletronicoApplication.java
@@ -0,0 +1,13 @@
+package br.futurodev.joinville.coletaeletronico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColetaeletronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColetaeletronicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/futurodev/joinville/coletaeletronico/ColetaeletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.futurodev.joinville.coletaeletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColetaeletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
